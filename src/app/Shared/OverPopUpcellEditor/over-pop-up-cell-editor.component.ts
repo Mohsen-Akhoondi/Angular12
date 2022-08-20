01c94245e317c3b7971e232f222343f66d93ea6c
FATEME SHOMALNASAB <shomalnasab@example.com>
2022-08-20 09:38:46 +0430

    sh

--- src/app/Shared/OverPopUpcellEditor/over-pop-up-cell-editor.component.ts
+++ src/app/Shared/OverPopUpcellEditor/over-pop-up-cell-editor.component.ts
@@ -11,13 +11,17 @@ export class OverPopUpCellEditorComponent implements ICellEditorAngularComp, OnI
   BeginSearch;
   PopupParam;
   SearchPopupType;
+  MainMinwidthPixel = 1240;
+  MainMaxwidthPixel = 1240;
+  startLeftPosition = 60;
+  startTopPosition = 10;
   constructor() { }
   @ViewChild('inputRef') inputRefElement;
   ngOnInit() {
   }
   getValue() {
     if (this.InputValue) {
-    return this.InputValue;
+      return this.InputValue;
     } else { return ''; }
   }
   isPopup?(): boolean {
@@ -33,14 +37,21 @@ export class OverPopUpCellEditorComponent implements ICellEditorAngularComp, OnI
   }
   focusOut?(): void {
   }
-  agInit (params) {
+  agInit(params) {
     this.InputValue = params.value;
     this.SearchPopupType = params.SearchPopupType;
     if (params.PopupParam) {
-    this.PopupParam = params.PopupParam;
+      this.PopupParam = params.PopupParam;
     }
+
   }
   dosearch() {
+    if (this.SearchPopupType === 'product-pattern' && !this.PopupParam.ShowGrid) {
+      this.PopupParam.SearchSTR = this.InputValue
+      this.MainMinwidthPixel = null;
+      this.MainMaxwidthPixel = null;
+      this.startLeftPosition = 440;
+    }
     this.BeginSearch = true;
   }
   getOutPutParam(event) {
